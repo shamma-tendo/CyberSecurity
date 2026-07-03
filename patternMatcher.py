5f511711254f732157e7054c7e31210e323298dc
muteesasira tendo shamma <shammatendo1@gmail.com>
2026-07-03 16:59:18 +0000

    code it

---
+++ patternMatcher.py
@@ -0,0 +1,12 @@
+from scapy.all import ARP, sniff
+
+def process_packet(packet):
+    if packet.haslayer(ARP) and packet[ARP].op == 2:  # is-at (response)
+        try:
+            real_mac = packet[ARP].hwsrc
+            response_mac = packet[ARP].hwsrc
+            print(f"ARP reply: {packet[ARP].psrc} is-at {real_mac}")
+        except IndexError:
+            pass
+
+# sniff(store=False, filter="arp", prn=process_packet)  # run with admin/root
---
+++ steganography.py
@@ -0,0 +1,22 @@
+from PIL import Image
+
+def encode_message(image_path, message, output_path):
+    img = Image.open(image_path)
+    encoded = img.copy()
+    width, height = img.size
+    message += chr(0)  # null terminator
+    binary_msg = ''.join(format(ord(c), '08b') for c in message)
+
+    data_index = 0
+    pixels = encoded.load()
+    for y in range(height):
+        for x in range(width):
+            if data_index < len(binary_msg):
+                r, g, b = pixels[x, y][:3]
+                r = (r & ~1) | int(binary_msg[data_index])
+                pixels[x, y] = (r, g, b)
+                data_index += 1
+    encoded.save(output_path)
+    print(f"Message hidden in {output_path}")
+
+# encode_message("input.png", "meet at midnight", "output.png")
